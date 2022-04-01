831f95686ae5bbaebfff61d2d23b1167090a1558
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-04-01 12:42:44 +0200

    updated foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,64 @@
-let pokemonList = [
-    {
-        name: "Charmander",
-        height: 0.6,
-        types: ["Fire"],
-    },
-    {
-        name: "Bulbasaur",
-        height: 0.7,
-        types: ["Grass", "Poison"],
-    },
-    {
-        name: "Squirtle",
-        height: 0.5,
-        types: ["Water"],
-    },
-]
+let pokemonRepository = (function(){
+    let pokemonList = [
+        {
+            name: "Charmander",
+            height: 0.6,
+            types: ["Fire"],
+        },
+        {
+            name: "Bulbasaur",
+            height: 0.7,
+            types: ["Grass", "Poison"],
+        },
+        {
+            name: "Squirtle",
+            height: 0.5,
+            types: ["Water"],
+        },
+    ];
+   
+   return{
+    add: function(pokemon){
+        pokemonList.push(pokemon);
+      },
+    
+    getAll: function(){
+        return pokemonList;
+    }
+   };
+})();
 
-pokemonList.forEach(function(name)  {
-    console.log(name);
-});
+pokemonRepository.getAll().forEach(function(pokemon)   {
+    document.write("Name: " + pokemon.name + " (Height: " + pokemon.height + ". Types: " + pokemon.types + ".)");
+})
+
+
+
+
 
 
 //for (let i = 0; i < pokemonList.length; i++){
    
 //console.log(pokemonList[i].name)
 
-if(pokemonList[i].height >= 0.6){
-    document.write(pokemonList[i].name + " Height " + pokemonList[i].height + "" + "Wow its a big Pokemon!")
-} else {
-    document.write(pokemonList[i].name + " Height " + pokemonList[i].height)
-}
+//if(pokemonList[i].height >= 0.6){
+ //   document.write(pokemonList[i].name + " Height " + pokemonList[i].height + "" + "Wow its a big Pokemon!")
+//} else {
+//    document.write(pokemonList[i].name + " Height " + pokemonList[i].height)
+//}
 
 //document.write("Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height) 
-}
+//}
     
 
 
 
 
-function divide(dividend,divisor){
-    if(divisor === 0)
-        return "You´re trying to divide by zero."
-}else{
-    let result = dividend / divisor;
-    return result;
-}
-}
+//function divide(dividend,divisor){
+   // if(divisor === 0)
+    //    return "You´re trying to divide by zero."
+//}else{
+  //  let result = dividend / divisor;
+    //return result;
+//}
+//}
